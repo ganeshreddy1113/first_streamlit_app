80db0237cc46b266b3f428ce6dc84b2539c57fea
ganeshreddy1113 <109714890+ganeshreddy1113@users.noreply.github.com>
2022-07-21 10:47:10 +0530

    Update streamlit_app.py
    reading an csv file using pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,4 +7,9 @@ streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Roket Smoothie')
 streamlit.text('🐔 Hard_Boiled Free_Range Egg')
 streamlit.text('🥑🍞Avacado Toast')              
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')              
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')   
+
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
