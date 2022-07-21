b3f81afbf0e6fd04be09e9be27c110a1449e0b7a
ganeshreddy1113 <109714890+ganeshreddy1113@users.noreply.github.com>
2022-07-21 10:34:58 +0530

    Update streamlit_app.py
    updated text while getting an error

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 streamlit.title('My Parents Healthy Diner')
 streamlit.header('Breakfast Menu')
-strealit.text('Omega 3 & Blueberry Oatmeal)
+strealit.text('Omega 3 & Blueberry Oatmeal')
 strealit.text('Kale, Spinach & Roket Smoothie')
 streamlit.text('Hard_Boiled Free_Range Egg')              
               
