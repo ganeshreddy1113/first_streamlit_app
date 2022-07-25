22c57beadecc28afe2b1e7ad40eae6e04562627c
ganeshreddy1113 <109714890+ganeshreddy1113@users.noreply.github.com>
2022-07-25 10:28:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,4 +37,5 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 import snowflake.connector 
+cffi==1.15.0
                       
